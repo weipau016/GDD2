0221c39cdb38719e8ec05497e154c47dd1d7ba66
Paul Weissensteiner <paul.weissensteiner@student.tugraz.at>
2021-05-10 19:35:04 +0200

    New setup with VR Template

---
+++ Source/GDD2/GDD2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GDD2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GDD2, "GDD2" );
