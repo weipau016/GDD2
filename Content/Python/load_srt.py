01c09118e6c853cf3158231e99729811d581c1d5
Irena Ruprecht <irena.ruprecht@gmail.com>
2021-05-31 12:09:53 +0200

    remove hardcoded file path in python loader

--- Content/Python/load_srt.py
+++ Content/Python/load_srt.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join, isdir
 import os
 
-path_to_project = 'C:/Users/renowsky/Documents/TU/GDD2/WrongDirection/Project/GDD2'
+path_to_project = __file__[0: len(__file__) - len('Content/Python/load_srt.py')]
 
 class Sequence :
     def __init__(self, index, start, duration, text):
